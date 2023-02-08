9940337489a8bebd0cc28bd0a21227dae5895054
Moad Tellami <mtellami@e1r6p1.1337.ma>
2023-02-08 10:38:47 +0100

    enhance

--- microshell.c
+++ microshell.c
@@ -5,32 +5,102 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtellami <mtellami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/11 13:36:00 by mtellami          #+#    #+#             */
-/*   Updated: 2023/02/02 16:16:15 by mtellami         ###   ########.fr       */
+/*   Created: 2023/02/08 07:28:57 by mtellami          #+#    #+#             */
+/*   Updated: 2023/02/08 10:36:15 by mtellami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "microshell.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-t_data *newnode(char **args, char *sep)
+typedef struct s_list
 {
-	t_data  *new = malloc(sizeof(t_data));
+	char **args;
+	char *sep;
+	struct s_list *next;
+}	t_list;
+
+int ft_strlen(char *str)
+{
+	int i = -1;
+	while (str[++i]);
+	return (i);
+}
+
+int ft_tabsize(char **tab)
+{
+	int i = -1;
+	while (tab[++i]);
+	return (i);
+}
+
+void ft_freearr(char **arr)
+{
+	int  i = -1;
+	while (arr[++i])
+		free(arr[i]);
+	free(arr);
+}
+
+void err_fatal(void)
+{
+	char *msg = "error: fatal\n";
+	write(2, msg, ft_strlen(msg));
+	exit(1);
+}
+
+char *ft_strdup(char *str)
+{
+	char *dup = malloc(ft_strlen(str) + 1);
+	if (!dup)
+		return (NULL);
+	int i = -1;
+	while (str[++i])
+		dup[i] = str[i];
+	dup[i] = 0;
+	return (dup);
+}
+
+char **concate(char **tab, char *str)
+{
+	if (!tab)
+	{
+		tab = malloc(sizeof(char *));
+		*tab = NULL;
+	}
+	char **buffer = malloc(sizeof(char *) * (ft_tabsize(tab) + 2));
+	if (!buffer)
+		return (NULL);
+	int i = -1;
+	while (tab[++i])
+		buffer[i] = ft_strdup(tab[i]);
+	buffer[i++] = ft_strdup(str);
+	buffer[i] = NULL;
+	ft_freearr(tab);
+	return (buffer);
+}
+
+t_list *newnode(char **args, char *sep)
+{
+	t_list *new = malloc(sizeof(t_list));
 	if (!new)
-		err_fatal();
+		return (NULL);
 	new->args = args;
 	new->sep = sep;
 	new->next = NULL;
 	return (new);
 }
 
-t_data	*lastnode(t_data *lst)
+t_list *lastnode(t_list *lst)
 {
 	while (lst->next)
 		lst = lst->next;
 	return (lst);
 }
 
-void	addback(t_data **lst, t_data  *node)
+void addback(t_list **lst, t_list *node)
 {
 	if (*lst)
 		lastnode(*lst)->next = node;
@@ -38,67 +108,68 @@ void	addback(t_data **lst, t_data  *node)
 		*lst = node;
 }
 
-t_data	*init_list(int ac, char **av)
+t_list *init_list(int ac, char **av)
 {
-	t_data *lst = NULL;
+	t_list *lst = NULL;
 	int i = 1;
 	while (i < ac)
 	{
 		char **buffer = NULL;
-		while (i < ac && strcmp(av[i], "|") && strcmp(av[i], ";"))
-			buffer = arr_concate(buffer, av[i++]);
-		t_data *node = newnode(buffer, av[i]);
-		addback(&lst, node);
-		i++;
+		while (i < ac && strcmp(av[i], ";") && strcmp(av[i], "|"))
+			buffer = concate(buffer, av[i++]);
+		addback(&lst, newnode(buffer, av[i++]));
+		while (i < ac && !strcmp(av[i], ";"))
+			i++;
 	}
 	return (lst);
 }
 
-void	re_cd(t_data *data)
+void re_cd(char **args)
 {
-	if (ft_tabsize(data->args) != 2)
-		write(2, "error: cd: bad arguments\n", 26);
+	if (ft_tabsize(args) != 2)
+		write(2, "error: cd: bad arguments\n", 25);
 	else
 	{
-		if ((chdir(data->args[1])) == -1)
+		if (chdir(args[1]))
 		{
 			write(2, "error: cd: cannot change directory to ", 38);
-			write(2, data->args[1], ft_strlen(data->args[1]));
+			write(2, args[1], ft_strlen(args[1]));
 			write(2, "\n", 1);
 		}
 	}
 }
 
-void	execution(t_data *lst, char **env)
+void execution(char **args, char **env)
 {
-	if ((execve(lst->args[0], lst->args, env)) == -1)
+
+	if (execve(args[0], args, env) == -1)
 	{
 		write(2, "error: cannot execute ", 22);
-		write(2, lst->args[0], ft_strlen(lst->args[0]));
+		write(2, args[0], ft_strlen(args[0]));
 		write(2, "\n", 1);
 		exit(1);
 	}
 }
 
-void	multi_pipe(t_data *lst, char **env)
+void multi_pipe(t_list *lst, char **env)
 {
-	int	fd[2];
+	int fd[2];
 	if (!strcmp(lst->args[0], "cd"))
-		re_cd(lst);
+		re_cd(lst->args);
 	else
 	{
 		if (pipe(fd) == -1)
 			err_fatal();
 		int pid = fork();
-		if (pid == -1)
+		if (pid  == -1)
 			err_fatal();
-		if (!pid)
+		else if (!pid)
 		{
 			close(fd[0]);
 			if (dup2(fd[1], 1) == -1)
 				err_fatal();
 			close(fd[1]);
-			execution(lst, env);
+			execution(lst->args, env);
 		}
 		close(fd[1]);
 		if (dup2(fd[0], 0) == -1)
@@ -108,43 +179,38 @@ void	multi_pipe(t_data *lst, char **env)
 	}
 }
 
-void	exec_last(t_data *lst, char **env)
+void exec_last(t_list *lst, char **env)
 {
-	int stat;
 	if (!strcmp(lst->args[0], "cd"))
-		re_cd(lst);
+		re_cd(lst->args);
 	else
 	{
 		int pid = fork();
 		if (pid == -1)
 			err_fatal();
-		if (!pid)
-			execution(lst, env);
-		waitpid(pid, &stat, 0);
-		if (stat)
-			exit(stat);
+		else if (!pid)
+			execution(lst->args, env);
+		waitpid(pid, NULL, 0);
 	}
 }
 
-int	main(int ac, char **av, char **env)
+int main(int ac, char **av, char **env)
 {
-	t_data *lst = init_list(ac, av);
-	t_data *tmp = lst;
+	t_list *list = init_list(ac, av);
+	t_list *tmp = list;
+	int i = dup(0);
 	while (tmp)
 	{
-		int in = dup(0);
-		if (in == -1)
-			err_fatal();
 		while (tmp->sep && !strcmp(tmp->sep, "|"))
 		{
 			multi_pipe(tmp, env);
 			tmp = tmp->next;
 		}
-		if ((dup2(0, in)) == -1)
-			err_fatal();
-		close(in);
 		exec_last(tmp, env);
+		if (dup2(i, 0) == -1)
+			err_fatal();
 		tmp = tmp->next;
 	}
+	close(i);
 	return (0);
 }
